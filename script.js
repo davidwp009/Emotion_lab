37c0020840a988da19ae15e809f47b9f1bce6162
davidwp009 <142822982+davidwp009@users.noreply.github.com>
2026-02-20 17:04:16 +0100

    not

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
-const logo = document.querySelector('.head__logo');
-const video = document.querySelector('.logo-video');
+const hero = document.querySelector('.hero-content__back');
 
-logo.addEventListener('mouseenter', () => {
-    video.play();
-    video.style.opacity = "1";
-});
+// масив твоїх фонів
+const backgrounds = [
+  './images/projects/projects-7.svg',
+  './images/projects/projects-8.svg',
+  './images/projects/projects-9.svg'
+];
 
-logo.addEventListener('mouseleave', () => {
-    video.pause();
-    video.currentTime = 0;
-    video.style.opacity = "0";
-});
+let index = 0;
+
+setInterval(() => {
+  index = (index + 1) % backgrounds.length;
+  hero.style.backgroundImage = `url(${backgrounds[index]})`;
+}, 5000); // 5000 = 5 секунд
+
+
+
+
+
+
+
+
+
+newFunction();
+
+function newFunction() {
+    const logo = document.querySelector('.head__logo');
+    const video = document.querySelector('.logo-video');
+
+    logo.addEventListener('mouseenter', () => {
+        video.play();
+        video.style.opacity = "1";
+    });
+
+    logo.addEventListener('mouseleave', () => {
+        video.pause();
+        video.currentTime = 0;
+        video.style.opacity = "0";
+    });
+}
